f5b11e99c139bcacb687c48779c06ffa78273e06
Costa Huang <costa.huang@outlook.com>
2023-03-01 02:42:11 +0000

    change default num envs for sync version

--- cleanba/ppo_envpool_impala_atari_wrapper.py
+++ cleanba/ppo_envpool_impala_atari_wrapper.py
@@ -56,7 +56,7 @@ def parse_args():
         help="total timesteps of the experiments")
     parser.add_argument("--learning-rate", type=float, default=2.5e-4,
         help="the learning rate of the optimizer")
-    parser.add_argument("--num-envs", type=int, default=60,
+    parser.add_argument("--num-envs", type=int, default=120,
         help="the number of parallel game environments")
     parser.add_argument("--num-steps", type=int, default=128,
         help="the number of steps to run in each environment per policy rollout")
